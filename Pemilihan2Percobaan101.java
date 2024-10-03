d12a3b36b76a412cbeb3a7e2773a9b09bc27fe3f
afrizalrfli <tkj1211afrizalrafli@smkn1glagah.sch.id>
2024-10-03 13:57:34 +0700

    modified:   Pemilihan2Percobaan101.java

--- Pemilihan2Percobaan101.java
+++ Pemilihan2Percobaan101.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan101 {
     public static void main(String[] args) {
         Scanner input01 = new Scanner (System.in);
-        
+
+         //INPUT
         System.out.print("Masukkan tahun: ");
         int tahun = input01.nextInt();
-
+        //LOGIKA TAHUN KABISAT
         if ((tahun % 4) == 0) {
             if (( tahun % 100) != 0)
             System.out.println("Tahun Kabisat");
         }else{
-            if (( tahun % 400) != 0)
+            if (( tahun % 400) == 0)
             System.out.println("Tahun Kabisat");
             else
-            System.out.println("Bukn Tahun Kabisat");
+            System.out.println("Bukan Tahun Kabis");
         }
         System.out.println("Bukan Tahun Kabisat");
     }
